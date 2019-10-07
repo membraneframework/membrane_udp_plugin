c5e02e3860e7c0091e75f7835ca060318f2d00a9
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2019-10-07 12:51:12 +0200

    Fix moved type

--- lib/membrane_element_udp/common_socket_behaviour.ex
+++ lib/membrane_element_udp/common_socket_behaviour.ex
@@ -4,14 +4,14 @@ defmodule Membrane.Element.UDP.CommonSocketBehaviour do
   import Mockery.Macro
 
   alias Membrane.Element
-  alias Membrane.Element.Base.Mixin.CommonBehaviour
+  alias Membrane.Element.Base
   alias Membrane.Element.CallbackContext.PlaybackChange
   alias Membrane.Element.UDP.Socket
 
   @spec handle_stopped_to_prepared(
           context :: PlaybackChange.t(),
           state :: Element.state_t()
-        ) :: CommonBehaviour.callback_return_t()
+        ) :: Base.callback_return_t()
   def handle_stopped_to_prepared(_context, %{local_socket: local_socket} = state) do
     case mockable(Socket).open(local_socket) do
       {:ok, socket} -> {:ok, %{state | local_socket: socket}}
@@ -22,7 +22,7 @@ defmodule Membrane.Element.UDP.CommonSocketBehaviour do
   @spec handle_prepared_to_stopped(
           context :: PlaybackChange.t(),
           state :: Element.state_t()
-        ) :: CommonBehaviour.callback_return_t()
+        ) :: Base.callback_return_t()
   def handle_prepared_to_stopped(_context, %{local_socket: local_socket} = state) do
     updated_socket = mockable(Socket).close(local_socket)
     {:ok, %{state | local_socket: updated_socket}}
