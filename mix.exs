41aea78402966c9e435a28a55741b85304e0f81d
Dominik <dominikstanaszek@icloud.com>
2020-02-11 15:21:06 +0100

    Bump version to 0.3.2

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.Element.UDP.MixProject do
   use Mix.Project
 
-  @version "0.3.1"
+  @version "0.3.2"
   @github_url "https://github.com/membraneframework/membrane-element-udp"
 
   def project do
