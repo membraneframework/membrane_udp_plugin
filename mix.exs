3d99fa05032a991e3f404481f2efd249f9285a44
Daniel Jodłoś <daniel.jodlos@swmansion.com>
2022-09-05 08:34:13 +0200

    Update CI config and bump deps (#18)

--- mix.exs
+++ mix.exs
@@ -15,7 +15,8 @@ defmodule Membrane.UDP.MixProject do
       name: "Membrane UDP plugin",
       source_url: @github_url,
       docs: docs(),
-      deps: deps()
+      deps: deps(),
+      dialyzer: dialyzer()
     ]
   end
 
@@ -57,5 +58,18 @@ defmodule Membrane.UDP.MixProject do
       {:dialyxir, "~> 1.1", only: :dev, runtime: false},
       {:credo, "~> 1.5", only: :dev, runtime: false}
     ]
+  end
+
+  defp dialyzer() do
+    opts = [
+      flags: [:error_handling]
+    ]
+
+    if System.get_env("CI") == "true" do
+      # Store PLTs in cacheable directory for CI
+      [plt_local_path: "priv/plts", plt_core_path: "priv/plts"] ++ opts
+    else
+      opts
+    end
   end
 end
